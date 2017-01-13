2965dffdbc42a73e6d581f5131f7a3669dbb4788
mirraico <feixuan2013@gmail.com>
2017-01-13 13:26:42 +0800

    MOD: binaryTreeZigzagLevelOrderTraversal

--- binaryTreeZigzagLevelOrderTraversal/BinaryTreeZigzagLevelOrderTraversal.java
+++ binaryTreeZigzagLevelOrderTraversal/BinaryTreeZigzagLevelOrderTraversal.java
@@ -9,24 +9,22 @@ public class BinaryTreeZigzagLevelOrderTraversal {
 		if(root == null) return ans;
 		LinkedList<TreeNode> queue = new LinkedList<>();
 		queue.addLast(root);
-		int thisNum = 1, nextNum = 0;
 		boolean reverse = false;
 		while(!queue.isEmpty()) {
 			LinkedList<Integer> tmpAns = new LinkedList<>();
-			while((thisNum--) != 0) {
+			int levelNum = queue.size();
+			while((levelNum--) != 0) {
 				TreeNode tn = queue.removeFirst();
 				if(reverse) tmpAns.addFirst(tn.val);
 				else tmpAns.addLast(tn.val);
 				if(tn.left != null) {
 					queue.addLast(tn.left);
-					nextNum++;
 				}
 				if(tn.right != null) {
 					queue.addLast(tn.right);
-					nextNum++;
 				}
 			}
-			thisNum = nextNum; nextNum = 0; reverse = !reverse;
+			reverse = !reverse;
 			ans.add(tmpAns);
 		}
 		return ans;
