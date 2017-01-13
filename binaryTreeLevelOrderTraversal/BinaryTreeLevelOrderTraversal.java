2963f899cc8aeb8d1ac3f8e4a2ce9cdff41708a8
mirraico <feixuan2013@gmail.com>
2017-01-13 13:24:54 +0800

    MOD: binaryTreeLevelOrderTraversal

--- binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
+++ binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -2,29 +2,26 @@ package com.mirraico.leetcode;
 
 import java.util.*;
 
-public class BinaryTreeLevelOrderTraversal {
+public class Binary TreeLevelOrderTraversal {
 	
 	public List<List<Integer>> levelOrder(TreeNode root) {
 		List<List<Integer>> ans = new ArrayList<>();
 		if(root == null) return ans;
 		LinkedList<TreeNode> queue = new LinkedList<>();
 		queue.addLast(root);
-		int thisNum = 1, nextNum = 0;
 		while(!queue.isEmpty()) {
 			List<Integer> tmpAns = new ArrayList<>();
-			while((thisNum--) != 0) {
+			int levelNum = queue.size();
+			while((levelNum--) != 0) {
 				TreeNode tn = queue.removeFirst();
 				tmpAns.add(tn.val);
 				if(tn.left != null) {
 					queue.addLast(tn.left);
-					nextNum++;
 				}
 				if(tn.right != null) {
 					queue.addLast(tn.right);
-					nextNum++;
 				}
 			}
-			thisNum = nextNum; nextNum = 0;
 			ans.add(tmpAns);
 		}
 		return ans;
